34f54a44b9486f808d1b9a4edc0f079484d4519b
Prashanth Reddy R K <108233632+rkprashanthreddy@users.noreply.github.com>
2022-06-27 00:03:36 +0530

    Delete 9.AreaOfCircle.py

--- 9.AreaOfCircle.py
+++
@@ -1,4 +0,0 @@
-from math import pi
-radius = float(input("Enter The Radius Of The Circle:"))
-print("Area Of The Circle With Radius ", radius, " is ", pi*radius**2)
-print("Area Of The Circle With Radius"+str(radius)+" is "+str(pi*radius**2))
